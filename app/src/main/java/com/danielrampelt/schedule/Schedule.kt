e8d7b5517ed598c0208752c42ff37f77926c3407
Sternbach-Software <60489781+Sternbach-Software@users.noreply.github.com>
2023-08-18 07:38:56 -0400

    Update and rename MainActivity.kt to Schedule.kt

---
+++ app/src/main/java/com/danielrampelt/schedule/Schedule.kt
@@ -0,0 +1,182 @@
+package com.danielrampelt.schedule
+
+package com.neuropsycbaltimore.planner
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.ParentDataModifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.until
+import kotlin.math.roundToInt
+
+
+data class Event(
+    val name: String,
+    val color: Color,
+    val start: LocalDateTime,
+    val end: LocalDateTime,
+    val description: String? = null,
+)
+/**
+ * Formats a [LocalDateTime] in the format h:mm a.
+ * */
+fun LocalDateTime.format() =
+    "${if(hour == 0) 12 else hour % 12}:${minute.toString().padStart(2, '0')} ${if (hour < 12) "AM" else "PM"}"
+
+@Composable
+fun BasicEvent(
+    event: Event,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(end = 2.dp, bottom = 2.dp)
+            .background(event.color, shape = RoundedCornerShape(4.dp))
+            .padding(4.dp)
+    ) {
+        Text(
+            text = "${event.start.format()} - ${event.end.format()}",
+            style = MaterialTheme.typography.caption,
+        )
+
+        Text(
+            text = event.name,
+            style = MaterialTheme.typography.body1,
+            fontWeight = FontWeight.Bold,
+        )
+
+        if (event.description != null) {
+            Text(
+                text = event.description,
+                style = MaterialTheme.typography.body2,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        }
+    }
+}
+
+private val sampleEvents = listOf(
+    Event(
+        name = "Google I/O Keynote",
+        color = Color(0xFFAFBBF2),
+        start = LocalDateTime.parse("2021-05-18T09:00:00"),
+        end = LocalDateTime.parse("2021-05-18T11:00:00"),
+        description = "Tune in to find out about how we're furthering our mission to organize the world’s information and make it universally accessible and useful.",
+    ),
+    Event(
+        name = "Developer Keynote",
+        color = Color(0xFFAFBBF2),
+        start = LocalDateTime.parse("2021-05-18T11:15:00"),
+        end = LocalDateTime.parse("2021-05-18T12:15:00"),
+        description = "Learn about the latest updates to our developer products and platforms from Google Developers.",
+    ),
+    Event(
+        name = "What's new in Android",
+        color = Color(0xFF1B998B),
+        start = LocalDateTime.parse("2021-05-18T12:30:00"),
+        end = LocalDateTime.parse("2021-05-18T15:00:00"),
+        description = "In this Keynote, Chet Haase, Dan Sandler, and Romain Guy discuss the latest Android features and enhancements for developers.",
+    ),
+    Event(
+        name = "What's new in Machine Learning",
+        color = Color(0xFFF4BFDB),
+        start = LocalDateTime.parse("2021-05-19T09:30:00"),
+        end = LocalDateTime.parse("2021-05-19T11:00:00"),
+        description = "Learn about the latest and greatest in ML from Google. We’ll cover what’s available to developers when it comes to creating, understanding, and deploying models for a variety of different applications.",
+    ),
+    Event(
+        name = "What's new in Material Design",
+        color = Color(0xFF6DD3CE),
+        start = LocalDateTime.parse("2021-05-19T11:00:00"),
+        end = LocalDateTime.parse("2021-05-19T12:15:00"),
+        description = "Learn about the latest design improvements to help you build personal dynamic experiences with Material Design.",
+    ),
+    Event(
+        name = "Jetpack Compose Basics",
+        color = Color(0xFF1B998B),
+        start = LocalDateTime.parse("2021-05-20T12:00:00"),
+        end = LocalDateTime.parse("2021-05-20T13:00:00"),
+        description = "This Workshop will take you through the basics of building your first app with Jetpack Compose, Android's new modern UI toolkit that simplifies and accelerates UI development on Android.",
+    ),
+)
+
+private class EventDataModifier(
+    val event: Event,
+) : ParentDataModifier {
+    override fun Density.modifyParentData(parentData: Any?) = event
+}
+
+private fun Modifier.eventData(event: Event) = this.then(EventDataModifier(event))
+
+@Composable
+fun Schedule(
+    events: List<Event>,
+    modifier: Modifier = Modifier,
+    eventContent: @Composable (event: Event) -> Unit = { BasicEvent(event = it) },
+    minDate: LocalDate = events.minByOrNull(Event::start)!!.start.date,
+    maxDate: LocalDate = events.maxByOrNull(Event::end)!!.end.date,
+    timeZone: TimeZone = TimeZone.currentSystemDefault(),
+) {
+    val hourHeight = 64.dp
+    val dayWidth = 256.dp
+    val numDays = minDate.until(maxDate, DateTimeUnit.DAY) + 1
+    Layout(
+        content = {
+              events.sortedBy(Event::start).forEach { event ->
+                  Box(modifier = Modifier.eventData(event)) {
+                      eventContent(event)
+                  }
+              }
+        },
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+            .horizontalScroll(rememberScrollState())
+    ) { measureables, constraints ->
+        val height = hourHeight.roundToPx() * 24
+        val width = dayWidth.roundToPx() * numDays
+        val placeablesWithEvents = measureables.map { measurable ->
+            val event = measurable.parentData as Event
+            val eventDurationMinutes = event.start.toInstant(timeZone).until(event.end.toInstant(timeZone), DateTimeUnit.MINUTE)
+            val eventHeight = ((eventDurationMinutes / 60f) * hourHeight.toPx()).roundToInt()
+            val placeable = measurable.measure(constraints.copy(minWidth = dayWidth.roundToPx(), maxWidth = dayWidth.roundToPx(), minHeight = eventHeight, maxHeight = eventHeight))
+            Pair(placeable, event)
+        }
+        val todayStartOfDay = Clock.System.now().toLocalDateTime(timeZone).date.atStartOfDayIn(timeZone)
+        layout(width, height) {
+            placeablesWithEvents.forEach { (placeable, event) ->
+                val start = event.start.toInstant(timeZone)
+                val eventOffsetMinutes =    todayStartOfDay                 .until(start, DateTimeUnit.MINUTE)
+                val eventY = ((eventOffsetMinutes / 60f) * hourHeight.toPx()).roundToInt()
+                val eventOffsetDays =       minDate.atStartOfDayIn(timeZone).until(start, DateTimeUnit.DAY, timeZone).toInt()
+                val eventX = eventOffsetDays * dayWidth.roundToPx()
+                placeable.place(eventX, eventY)
+            }
+        }
+    }
+}
